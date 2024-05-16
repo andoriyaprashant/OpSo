46ebda9d21867a01a39782dd596ac73fc4bf276a
Kajal Naik <naikkajal0603@gmail.com>
2024-05-16 21:54:05 +0530

    gssoc ui

--- lib/programs screen/google_summer_of_code_screen.dart
+++ lib/programs screen/google_summer_of_code_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: GoogleSummerOfCodeScreen(),
+  ));
+}
+
 class GoogleSummerOfCodeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,64 +19,109 @@ class GoogleSummerOfCodeScreen extends StatelessWidget {
         children: [
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 22, bottom: 14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                    ),
+                    onChanged: (value) {
+                      // Handle search input
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: SizedBox(height: 10),
+                  ), // Adjust as needed
+                  Text(
+                    'Google Summer of Code is a global, online program focused on bringing new contributors into open source software development.',
+                    style: TextStyle(fontSize: 19,fontWeight: FontWeight.w600, color: const Color.fromARGB(255, 75, 40, 136)),
+                    
+                  ),
+                ],
               ),
-              onChanged: (value) {
-                // Handle search input
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                launch('https://summerofcode.withgoogle.com/programs/2024/projects');
               },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              icon: Icon(Icons.assignment),
+              label: Text('View Projects(2024)'),
             ),
           ),
           SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              YearButton(
-                year: '2021',
-                url: 'https://summerofcode.withgoogle.com/archive/2021/organizations', // Replace with actual URL
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                launch('https://summerofcode.withgoogle.com/programs/2024/organizations');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
               ),
-              YearButton(
-                year: '2022',
-                url: 'https://summerofcode.withgoogle.com/archive/2022/organizations', // Replace with actual URL
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              YearButton(
-                year: '2023',
-                url: 'https://summerofcode.withgoogle.com/archive/2023/organizations', // Replace with actual URL
-              ),
-              YearButton(
-                year: '2024',
-                url: 'https://summerofcode.withgoogle.com/archive/2024/organizations', // Replace with actual URL
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // launch('https://example.com/projects'); // Replace with actual URL
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 226, 230, 120), // Set button color
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              icon: Icon(Icons.business),
+              label: Text('View Organizations(2024)'),
             ),
-            child: Text('View Projects'),
           ),
+          buildPastProgramsText(),
+          SizedBox(height: 10),
+          buildYearButtonsRow(['2020', '2021']),
+          SizedBox(height: 30),
+          buildYearButtonsRow(['2022', '2023']),
         ],
       ),
     );
   }
+
+  Widget buildPastProgramsText() {
+    return Container(
+      margin: EdgeInsets.only(left: 55, top: 30, bottom: 15),
+      child: Text(
+        'Past Programs:',
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue.shade900,
+        ),
+      ),
+    );
+  }
+
+  Widget buildYearButtonsRow(List<String> years) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: years.map((year) {
+        return YearButton(
+          year: year,
+          url: 'https://summerofcode.withgoogle.com/archive/$year/organizations',
+        );
+      }).toList(),
+    );
+  }
 }
+
 
 class YearButton extends StatelessWidget {
   final String year;
@@ -82,19 +133,17 @@ class YearButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
-        // ignore: deprecated_member_use
-        // launch(url);
+        launch(url);
       },
       style: ElevatedButton.styleFrom(
-        backgroundColor: Color.fromARGB(255, 172, 207, 236), // Set button color
+        backgroundColor: Color.fromARGB(255, 172, 207, 236),
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
       ),
-      child: Text(year),
+      child: Text(
+        year,
+        style: TextStyle(fontSize: 18), 
+      ),
     );
   }
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: GoogleSummerOfCodeScreen(),
-  ));
-}
