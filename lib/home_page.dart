152ce91a980f2a5b4d467c2c85f4ccb9c52074b5
Ashutosh <134039280+AlexMercer00@users.noreply.github.com>
2024-05-18 10:14:11 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'dart:async';
+
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -102,6 +102,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: Drawer(
+
         backgroundColor: Colors.transparent,
         width: MediaQuery.of(context).size.width,
         child: BackdropFilter(
@@ -226,7 +227,8 @@ class _HomePageState extends State<HomePage> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => MajorLeagueHackingFellowship(),
+            builder: (context) => MajorLeagueHackingFellowship()
+
           ),
         );
         break;
@@ -242,13 +244,59 @@ class _HomePageState extends State<HomePage> {
 
         case 'Outreachy':
           Navigator.push(context, MaterialPageRoute(builder: (context) => OutReachy()));
-          break;
-        case 'Summer of Bitcoin' :
-          Navigator.pushNamed(context, "/summer_of_bitcoin");
-          break;
-        case 'Linux Foundation' :
-          Navigator.push(context, MaterialPageRoute(builder: (context) => LinuxFoundation()));
-          break;
+switch (program.title) {
+  case 'Google Summer of Code':
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GoogleSummerOfCodeScreen(),
+      ),
+    );
+    break;
+
+  case 'Google Season of Docs':
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GoogleSeasonOfDocsScreen(),
+      ),
+    );
+    break;
+
+  case 'Major League Hacking Fellowship':
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MajorLeagueHackingFellowship(),
+      ),
+    );
+    break;
+
+  case 'GirlScript Summer of Code':
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GSSOCScreen(),
+      ),
+    );
+    break;
+
+  case 'Outreachy':
+    Navigator.push(context, MaterialPageRoute(builder: (context) => OutReachy()));
+    break;
+
+  case 'Summer of Bitcoin':
+    Navigator.pushNamed(context, "/summer_of_bitcoin");
+    break;
+
+  case 'Linux Foundation':
+    Navigator.push(context, MaterialPageRoute(builder: (context) => LinuxFoundation()));
+    break;
+
+  default:
+    break;
+}
+
       default:
         break;
     }
@@ -267,4 +315,49 @@ class ProgramOption extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context)
+class ProgramOption extends StatelessWidget {
+  final String title;
+  final String imageAssetPath;
+  final VoidCallback onTap;
+
+  const ProgramOption({
+    required this.title,
+    required this.imageAssetPath,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 237, 237, 239),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              imageAssetPath,
+              width: 50,
+              height: 50,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Icon(Icons.arrow_forward),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
