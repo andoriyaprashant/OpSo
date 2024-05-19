167562b3b99fbb47a200e7b1bdd0fd2d225e7001
Saumya-28 <saumya18921@gmail.com>
2024-05-19 17:42:16 +0530

    Implemented Project Features for GSoC Page

--- lib/programs screen/google_summer_of_code_screen.dart
+++ lib/programs screen/google_summer_of_code_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:opso/widgets/gsoc/GsocProjectWidget.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 import '../modals/GSoC/Gsoc.dart';
 import '../services/ApiService.dart';
 import '../widgets/SearchandFilterWidget.dart';
@@ -39,6 +37,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
   ];
   List<Organization> orgList = [];
   late Future<void> _dataFetchFuture;
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
@@ -61,8 +60,15 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
         gsoc2024 = orgData2024.organizations ?? [];
         orgList = gsoc2024; // Default to the latest year
       });
+      _populateAnimatedList(orgList);
     } catch (e) {
       print('Error: $e');
+    }
+  }
+
+  void _populateAnimatedList(List<Organization> organizations) {
+    for (var i = 0; i < organizations.length; i++) {
+      _listKey.currentState?.insertItem(i);
     }
   }
 
@@ -71,6 +77,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
       orgList = _getOrganizationsByYear(selectedYear)
           .where((element) => element.technologies?.contains(searchTag) == true || element.topics?.contains(searchTag) == true)
           .toList();
+      _populateAnimatedList(orgList);
     });
   }
 
@@ -83,6 +90,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
             .where((element) => element.name?.toLowerCase().contains(searchText.toLowerCase()) == true)
             .toList();
       }
+      _populateAnimatedList(orgList);
     });
   }
 
@@ -126,7 +134,6 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                   TextFormField(
                     decoration: InputDecoration(
                       filled: true,
-                      // fillColor: const Color(0xFFEEEEEE),
                       hintText: 'Search',
                       suffixIcon: const Icon(Icons.search),
                       enabledBorder: OutlineInputBorder(
@@ -187,6 +194,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                               selectedYear = 2021;
                               orgList = gsoc2021;
                             });
+                            _populateAnimatedList(gsoc2021);
                           },
                           backgroundColor: selectedYear == 2021
                               ? Colors.white
@@ -200,6 +208,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                               selectedYear = 2022;
                               orgList = gsoc2022;
                             });
+                            _populateAnimatedList(gsoc2022);
                           },
                           backgroundColor: selectedYear == 2022
                               ? Colors.white
@@ -213,6 +222,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                               selectedYear = 2023;
                               orgList = gsoc2023;
                             });
+                            _populateAnimatedList(gsoc2023);
                           },
                           backgroundColor: selectedYear == 2023
                               ? Colors.white
@@ -226,6 +236,7 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                               selectedYear = 2024;
                               orgList = gsoc2024;
                             });
+                            _populateAnimatedList(gsoc2024);
                           },
                           backgroundColor: selectedYear == 2024
                               ? Colors.white
@@ -261,22 +272,11 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
                     ],
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: orgList.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          child: Column(
-                            children: [
-                              GsocProjectWidget(
-                                index: index + 1,
-                                modal: orgList[index],
-                                height: height * 0.2,
-                                width: width,
-                              ),
-                            ],
-                          ),
-                        );
+                    child: AnimatedList(
+                      key: _listKey,
+                      initialItemCount: orgList.length,
+                      itemBuilder: (context, index, animation) {
+                        return _buildAnimatedItem(context, index, animation, height, width);
                       },
                     ),
                   ),
@@ -285,6 +285,40 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildAnimatedItem(BuildContext context, int index, Animation<double> animation, double height, double width) {
+    return SizeTransition(
+      sizeFactor: animation,
+      axis: Axis.vertical,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              GsocProjectWidget(
+                index: index + 1,
+                modal: orgList[index],
+                height: height * 0.2,
+                width: width,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
