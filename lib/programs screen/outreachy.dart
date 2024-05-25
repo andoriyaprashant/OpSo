22d48f66ab0642a0a66926af6edeb3f40cb8b2d0
Saumya-28 <saumya18921@gmail.com>
2024-05-25 21:42:41 +0530

    Implemented Swipe Down Reload Feature for each Program Screen

--- lib/programs screen/outreachy.dart
+++ lib/programs screen/outreachy.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:opso/modals/book_mark_model.dart';
 
 
+
+
 class OutReachy extends StatefulWidget {
   const OutReachy({super.key});
+
 
   @override
   State<OutReachy> createState() => _OutReachyState();
 }
 
+
 class _OutReachyState extends State<OutReachy> {
   bool isBookmarked = true;
   String currectPage = "/outreachy";
   String currentProject = "Outreachy";
+
 
   @override
   void initState() {
@@ -21,50 +26,62 @@ class _OutReachyState extends State<OutReachy> {
   }
 
 
+
+
   Future<void> _checkBookmarkStatus() async {
     bool bookmarkStatus = await HandleBookmark.isBookmarked(currentProject);
     setState(() {
       isBookmarked = bookmarkStatus;
     });
   }
-
+  Future<void> _refresh() async {
+    setState(() {
+      //implement refresh logic here when ready
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child: Scaffold(
 
-      appBar: AppBar(
-        title: const Text('OpSo'),
-          actions: <Widget>[
-            IconButton(
-            icon: (isBookmarked)
-                ? const Icon(Icons.bookmark_add_rounded)
-                : const Icon(Icons.bookmark_add_outlined),
-            onPressed: () {
-              setState(() {
-                isBookmarked = !isBookmarked;
-              });
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(isBookmarked ? 'Bookmark added' : 'Bookmark removed'),
-                    duration: const Duration(seconds: 2), // Adjust the duration as needed
-                  ),
-                );
-              if(isBookmarked){
-                print("Adding");
-                HandleBookmark.addBookmark(currentProject, currectPage);
-              }
-              else{
-                print("Deleting");
-                HandleBookmark.deleteBookmark(currentProject);
-              }
-            },
-            )
-          ]
+
+        appBar: AppBar(
+            title: const Text('OpSo'),
+            actions: <Widget>[
+              IconButton(
+                icon: (isBookmarked)
+                    ? const Icon(Icons.bookmark_add_rounded)
+                    : const Icon(Icons.bookmark_add_outlined),
+                onPressed: () {
+                  setState(() {
+                    isBookmarked = !isBookmarked;
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(isBookmarked ? 'Bookmark added' : 'Bookmark removed'),
+                      duration: const Duration(seconds: 2), // Adjust the duration as needed
+                    ),
+                  );
+                  if(isBookmarked){
+                    print("Adding");
+                    HandleBookmark.addBookmark(currentProject, currectPage);
+                  }
+                  else{
+                    print("Deleting");
+                    HandleBookmark.deleteBookmark(currentProject);
+                  }
+                },
+              )
+            ]
         ),
 
-      body: const Center(
-        child: Text('Outreachy'),
+
+        body: const Center(
+          child: Text('Outreachy'),
+        ),
       ),
     );
   }
 }
+
--- lib/programs screen/summer_of_bitcoin.dart
+++ lib/programs screen/summer_of_bitcoin.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:opso/modals/sob_project_modal.dart';
 import 'package:opso/widgets/sob_project_widget.dart';
 import 'package:opso/widgets/year_button.dart';
 
+
 import '../widgets/SearchandFilterWidget.dart';
+
 
 class SummerOfBitcoin extends StatefulWidget {
   const SummerOfBitcoin({super.key});
 
+
   @override
   State<SummerOfBitcoin> createState() => _SummerOfBitcoinState();
 }
+
 
 class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
   List<SobProjectModal> sob2023 = [];
@@ -21,8 +26,10 @@ class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
   List<SobProjectModal> sob2021 = [];
   int selectedYear = 2023;
 
+
   List<SobProjectModal> projectList = [];
   Future<void>? getProjectFunction;
+
 
   Future<void> initializeProjectLists() async {
     String response =
@@ -32,6 +39,7 @@ class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
       sob2023.add(SobProjectModal.fromMap(data));
     }
     projectList = sob2023;
+
 
     response = await rootBundle.loadString('assets/projects/sob/sob2022.json');
     jsonList = await json.decode(response);
@@ -45,11 +53,13 @@ class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
     }
   }
 
+
   @override
   void initState() {
     getProjectFunction = initializeProjectLists();
     super.initState();
   }
+
 
   void searchTag(String searchTag) {
     projectList = projectList
@@ -58,6 +68,7 @@ class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
         .toList();
     setState(() {});
   }
+
 
   void search(String searchText) {
     if (searchText.isEmpty) {
@@ -102,193 +113,202 @@ class _SummerOfBitcoinState extends State<SummerOfBitcoin> {
     'LND',
     'Eclair'
   ];
-
+  Future<void> _refresh() async {
+    setState(() {
+      initializeProjectLists();
+      selectedYear = 2023;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.sizeOf(context).height;
     var width = MediaQuery.sizeOf(context).width;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Summer of Bitcoin'),
-      ),
-      body: FutureBuilder<void>(
-          future: getProjectFunction,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              return Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 46, vertical: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        hintText: 'Search',
-                        suffixIcon: const Icon(Icons.search),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFEEEEEE),
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFEEEEEE),
-                          ),
-                        ),
-                        disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFEEEEEE),
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFEEEEEE),
-                          ),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 12.0, horizontal: 20.0),
-                      ),
-                      onFieldSubmitted: (value) {
-                        search(value.trim());
-                      },
-                      onChanged: (value) {
-                        if (value.isEmpty) {
-                          search(value);
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      height: height * 0.2,
-                      width: width,
-                      child: GridView(
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.5 / 0.6,
-                          crossAxisSpacing: 15,
-                          mainAxisSpacing: 15,
-                        ),
-                        children: [
-                          YearButton(
-                            year: "2021",
-                            isEnabled: selectedYear == 2021 ? true : false,
-                            onTap: () {
-                              setState(() {
-                                projectList = sob2021;
-                                selectedYear = 2021;
-                              });
-                            },
-                            backgroundColor: selectedYear == 2021
-                                ? Colors.white
-                                : const Color.fromRGBO(255, 183, 77, 1),
-                          ),
-                          YearButton(
-                            year: "2022",
-                            isEnabled: selectedYear == 2022 ? true : false,
-                            onTap: () {
-                              setState(() {
-                                projectList = sob2022;
-                                selectedYear = 2022;
-                              });
-                            },
-                            backgroundColor: selectedYear == 2022
-                                ? Colors.white
-                                : const Color.fromRGBO(255, 183, 77, 1),
-                          ),
-                          YearButton(
-                            year: "2023",
-                            isEnabled: selectedYear == 2023 ? true : false,
-                            onTap: () {
-                              setState(() {
-                                projectList = sob2023;
-                                selectedYear = 2023;
-                              });
-                            },
-                            backgroundColor: selectedYear == 2023
-                                ? Colors.white
-                                : const Color.fromRGBO(255, 183, 77, 1),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Text(
-                          'Filter by Org:',
-                          style: TextStyle(fontWeight: FontWeight.w400),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              DropdownWidget(
-                                items: languages,
-                                hintText: 'Org',
-                                onChanged: (newValue) {
-                                  setState(() {
-                                    switch (selectedYear) {
-                                      case 2021:
-                                        projectList = sob2021;
-                                        break;
-                                      case 2022:
-                                        projectList = sob2022;
-                                        break;
-                                      case 2023:
-                                        projectList = sob2023;
-                                        break;
-                                    }
-                                    searchTag(newValue);
-                                  });
-                                  // Perform filtering based on selectedLanguage
-                                },
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Expanded(
-                      // width: width,
-                      child: ListView.builder(
-                        itemCount: projectList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: SobProjectWidget(
-                              modal: projectList[index],
-                              height: height * 0.2,
-                              width: width,
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Summer of Bitcoin'),
+        ),
+        body: FutureBuilder<void>(
+            future: getProjectFunction,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                return Padding(
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 46, vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: 'Search',
+                          suffixIcon: const Icon(Icons.search),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFEEEEEE),
                             ),
-                          );
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFEEEEEE),
+                            ),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFEEEEEE),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFEEEEEE),
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 12.0, horizontal: 20.0),
+                        ),
+                        onFieldSubmitted: (value) {
+                          search(value.trim());
+                        },
+                        onChanged: (value) {
+                          if (value.isEmpty) {
+                            search(value);
+                          }
                         },
                       ),
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return const Center(child: Text("Some error occured"));
-            }
-          }),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        height: height * 0.2,
+                        width: width,
+                        child: GridView(
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 1.5 / 0.6,
+                            crossAxisSpacing: 15,
+                            mainAxisSpacing: 15,
+                          ),
+                          children: [
+                            YearButton(
+                              year: "2021",
+                              isEnabled: selectedYear == 2021 ? true : false,
+                              onTap: () {
+                                setState(() {
+                                  projectList = sob2021;
+                                  selectedYear = 2021;
+                                });
+                              },
+                              backgroundColor: selectedYear == 2021
+                                  ? Colors.white
+                                  : const Color.fromRGBO(255, 183, 77, 1),
+                            ),
+                            YearButton(
+                              year: "2022",
+                              isEnabled: selectedYear == 2022 ? true : false,
+                              onTap: () {
+                                setState(() {
+                                  projectList = sob2022;
+                                  selectedYear = 2022;
+                                });
+                              },
+                              backgroundColor: selectedYear == 2022
+                                  ? Colors.white
+                                  : const Color.fromRGBO(255, 183, 77, 1),
+                            ),
+                            YearButton(
+                              year: "2023",
+                              isEnabled: selectedYear == 2023 ? true : false,
+                              onTap: () {
+                                setState(() {
+                                  projectList = sob2023;
+                                  selectedYear = 2023;
+                                });
+                              },
+                              backgroundColor: selectedYear == 2023
+                                  ? Colors.white
+                                  : const Color.fromRGBO(255, 183, 77, 1),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text(
+                            'Filter by Org:',
+                            style: TextStyle(fontWeight: FontWeight.w400),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                DropdownWidget(
+                                  items: languages,
+                                  hintText: 'Org',
+                                  onChanged: (newValue) {
+                                    setState(() {
+                                      switch (selectedYear) {
+                                        case 2021:
+                                          projectList = sob2021;
+                                          break;
+                                        case 2022:
+                                          projectList = sob2022;
+                                          break;
+                                        case 2023:
+                                          projectList = sob2023;
+                                          break;
+                                      }
+                                      searchTag(newValue);
+                                    });
+                                    // Perform filtering based on selectedLanguage
+                                  },
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        // width: width,
+                        child: ListView.builder(
+                          itemCount: projectList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: SobProjectWidget(
+                                modal: projectList[index],
+                                height: height * 0.2,
+                                width: width,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(child: Text("Some error occured"));
+              }
+            }),
+      ),
     );
   }
 }
+
