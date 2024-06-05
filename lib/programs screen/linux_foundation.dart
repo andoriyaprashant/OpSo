44831f3f272d0387f4317c5eee4d239fd4177733
Saumya-28 <saumya18921@gmail.com>
2024-06-05 15:53:45 +0530

    Improved UI of Linux Foundation Screen

--- lib/programs screen/linux_foundation.dart
+++ lib/programs screen/linux_foundation.dart
@@ -74,7 +74,7 @@ class _LinuxFoundationState extends State<LinuxFoundation> {
     return RefreshIndicator(
       onRefresh: _refresh,
       child: Scaffold(
-        appBar: AppBar(title: const Text('OpSo'), actions: <Widget>[
+        appBar: AppBar(title: const Text('Linux Foundation'), actions: <Widget>[
           IconButton(
             icon: (isBookmarked)
                 ? const Icon(Icons.bookmark_add_rounded)
