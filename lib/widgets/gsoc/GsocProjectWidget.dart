a883a422f2c8aba90bc67589799e399be9816f95
samarsajad <142666229+samarsajad@users.noreply.github.com>
2024-07-29 20:22:17 +0530

    Gsoc Screen fixed

--- lib/widgets/gsoc/GsocProjectWidget.dart
+++ lib/widgets/gsoc/GsocProjectWidget.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 import '../../modals/GSoC/Gsoc.dart';
 
 class GsocProjectWidget extends StatelessWidget {
-  final GsocModel modal;
+  final Organization modal;
   final double height;
   final double width;
   final int index;
-
   const GsocProjectWidget({
     Key? key,
     required this.modal,
@@ -16,14 +14,12 @@ class GsocProjectWidget extends StatelessWidget {
     this.height = 100,
     this.width = 100,
   }) : super(key: key);
-
   Future<void> _launchUrl(String url) async {
     final Uri uri = Uri.parse(url);
     if (!await launchUrl(uri)) {
       throw 'Could not launch $url';
     }
   }
-
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
@@ -32,9 +28,7 @@ class GsocProjectWidget extends StatelessWidget {
 
     return GestureDetector(
       onTap: () async {
-        if (modal.projectUrl != null) {
-          await _launchUrl(modal.projectUrl!);
-        }
+        await _launchUrl(modal.url);
       },
       child: Container(
         width: width,
@@ -53,91 +47,83 @@ class GsocProjectWidget extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "$index. ${modal.organization}",
+                "$index. ${modal.name}",
                 style: TextStyle(
                   color: Colors.orange,
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              if (modal.originalProjectProposal != null) ...[
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    "Description : ${modal.originalProjectProposal}",
-                    style: TextStyle(
-                      color: textColor,
-                    ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  "By ${modal.description}",
+                  style: TextStyle(
+                    color: textColor,
                   ),
                 ),
-              ],
-              if (modal.mentor != null) ...[
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    "Mentor: ${modal.mentor}",
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                ),
-              ],
-              if (modal.technicalWriter != null) ...[
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    "Technical Writer: ${modal.technicalWriter}",
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                ),
-              ],
+              ),
               Padding(
                 padding: const EdgeInsets.only(top: 20.0),
                 child: Wrap(
                   spacing: 10,
                   runSpacing: 10,
                   children: [
-                    if (modal.organizationUrl != null &&
-                        modal.organizationUrl!.isNotEmpty)
+                    if (modal.contactEmail.isNotEmpty)
                       ElevatedButton.icon(
                         onPressed: () {
-                          _launchUrl(modal.organizationUrl!);
+                          _launchUrl('mailto:${modal.contactEmail}');
+                        },
+                        icon: Icon(Icons.email, color: Colors.white),
+                        label: Text("Email"),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: Colors.orange,
+                        ),
+                      ),
+                    if (modal.blogUrl.isNotEmpty)
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          _launchUrl(modal.blogUrl);
                         },
                         icon: Icon(Icons.web, color: Colors.white),
-                        label: Text("Org Website"),
+                        label: Text("Blog"),
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.orange,
-                        ),
-                      ),
-                    if (modal.reportUrl != null && modal.reportUrl!.isNotEmpty)
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          _launchUrl(modal.reportUrl!);
-                        },
-                        icon: Icon(Icons.description, color: Colors.white),
-                        label: Text("Report"),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.orange,
-                        ),
-                      ),
-                    if (modal.acceptedProjectProposalUrl != null &&
-                        modal.acceptedProjectProposalUrl!.isNotEmpty)
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          _launchUrl(modal.acceptedProjectProposalUrl!);
-                        },
-                        icon: Icon(Icons.file_present, color: Colors.white),
-                        label: Text("Proposal"),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.orange,
+                          foregroundColor: Colors.white, backgroundColor: Colors.orange,
                         ),
                       ),
                   ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  runSpacing: 10,
+                  children: List.generate(
+                    modal.technologies.length,
+                        (index) => Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 249, 241, 226),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: IntrinsicWidth(
+                        stepWidth: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Center(
+                            child: Text(
+                              modal.technologies[index],
+                              style: TextStyle(
+                                color: Colors.orange,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
