a22fb7a358f400b6182f6b8e4685e1c8fdfab8bc
rishabh7923 <rishabhyadav7923@gmail.com>
2024-10-30 08:48:21 +0530

    Fixed small error in code related to recent changes

--- lib/programs screen/google_summer_of_code_screen.dart
+++ lib/programs screen/google_summer_of_code_screen.dart
@@ -65,11 +65,15 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
   }
 
   Future<List<Organization>> loadOrganizations(int year) async {
-    setState(() { _isRefreshing = true; });
+    setState(() {
+      _isRefreshing = true;
+    });
     String path = 'assets/projects/gsoc_org/gsoc${year}org.json';
     String response = await rootBundle.loadString(path);
 
-    setState(() { _isRefreshing = false; });
+    setState(() {
+      _isRefreshing = false;
+    });
     var decodedResponse = json.decode(response) as List;
     return decodedResponse.map((org) => Organization.fromJson(org)).toList();
   }
@@ -149,21 +153,14 @@ class _GoogleSummerOfCodeScreenState extends State<GoogleSummerOfCodeScreen> {
     return RefreshIndicator(
       onRefresh: _refresh,
       child: Scaffold(
-               appBar: AppBar(
-                 leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-         
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        centerTitle: true,
-          title: const Text('GSoC'),
-          actions: <Widget>[IconButton(
-
         appBar: AppBar(
-          title: const Text('Google Summer of Code'),
+          leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () => Navigator.of(context).pop()),
+          centerTitle: true,
+          title: const Text('GSoC'),
           actions: <Widget>[
             IconButton(
-
               icon: (isBookmarked)
                   ? const Icon(Icons.bookmark_add_rounded)
                   : const Icon(Icons.bookmark_add_outlined),
